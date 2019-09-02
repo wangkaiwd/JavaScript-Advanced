78e32bcb4a5adca8d519e7fd261fa57abe11e781
wangkaiwd <1162486176@qq.com>
2019-09-02 23:51:28 +0800

    feat: test code find error

--- principle/eventHub/test/index.ts
+++ principle/eventHub/test/index.ts
@@ -1,8 +1,7 @@
 import EventHub from '../src/index';
-
+//
 const eventHub = new EventHub();
-eventHub.on('test1', (data: unknown) => {
-  console.log('data', data);
-});
-
-eventHub.emit('test1', 'test1');
+// eventHub.on('test1', (data: unknown) => {
+//   console.log('data', data);
+// });
+// eventHub.emit('test1', 'test1');
