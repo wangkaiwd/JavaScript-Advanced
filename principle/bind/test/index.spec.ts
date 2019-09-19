38c5cb394d72aeab8fbc510b1f1d35e001fb4fb6
wangkaiwd <1162486176@qq.com>
2019-09-19 15:00:20 +0800

    feat: delete all old bind test case and add all new

--- principle/bind/test/index.spec.ts
+++ principle/bind/test/index.spec.ts
@@ -42,59 +42,47 @@ describe('myBind', () => {
       p2: number;
     }
 
-    const fn1 = function (this: any, p1: number, p2: number) {
+    const fn1 = function (this: Props, p1: number, p2: number): void {
       this.p1 = p1;
       this.p2 = p2;
     };
+    const fn2 = fn1.myBind(undefined, 1, 2);
+    const object = new (fn2 as any)();
+    assert.deepEqual(object, { p1: 1, p2: 2 });
+  });
+  it('new的时候绑定p1,p2,并且object是fn1的实例', () => {
+    interface Props {
+      p1: number;
+      p2: number;
+    }
+
+    const fn1 = function (this: Props, p1: number, p2: number): void {
+      this.p1 = p1;
+      this.p2 = p2;
+    };
+    fn1.prototype.sayHi = function () {};
+    const fn2 = fn1.myBind(undefined, 1, 2);
+    const object = new (fn2 as any)();
+    // isPrototypeOf: 方法用于测试一个对象是否存在于另一个对象的原型链上
+    assert.isTrue(fn1.prototype.isPrototypeOf(object));
+    assert.deepEqual(typeof object.sayHi, 'function');
+  });
+  it('不用new,但是用类似的对象', () => {
+    interface Props {
+      p1: number;
+      p2: number;
+    }
+
+    const fn1 = function (this: Props, p1: number, p2: number): void {
+      this.p1 = p1;
+      this.p2 = p2;
+    };
+    const object1 = new (fn1 as any)('x', 'y');
+    const fn2 = fn1.myBind(object1, 1, 2);
+    const object2 = fn2();
+    assert.isUndefined(object2);
+    assert.strictEqual(object1.p1, 1);
+    assert.strictEqual(object1.p2, 2);
   });
 });
-
-const test5 = (message: string) => {
-  console.log(message);
-  const fn1 = function (this: any, p1: any, p2: any) {
-    this.p1 = p1;
-    this.p2 = p2;
-  };
-  const fn2 = fn1.myBind(undefined, 1, 2);
-  const object = new (fn2 as any)();
-  console.assert(object.p1 === 1);
-  console.assert(object.p2 === 2);
-};
-const test6 = (message: string) => {
-  console.log(message);
-  const fn1 = function (this: any, p1: any, p2: any) {
-    this.p1 = p1;
-    this.p2 = p2;
-  };
-  fn1.prototype.sayHi = function () {};
-  const fn2 = fn1.myBind(undefined, 1, 2);
-  // FIXME:这里 new 会报错，暂时先将类型设置为any,因为没有找到更好的方法
-  // @see: https://stackoverflow.com/questions/43623461/new-expression-whose-target-lacks-a-construct-signature-in-typescript
-  const object = new (fn2 as any)();
-  console.assert(object.p1 === 1, 'test6-p1');
-  console.assert(object.p2 === 2, 'test6-p2');
-  // __proto__从来没有被包括在EcmaScript语言规范中，但是现代浏览器都实现了它。它不被推荐使用
-  // console.assert(object.__proto__ === fn1.prototype, 'test6-prototype');
-  // fn1.prototype 对象是否存在于object的原型链上
-  console.assert(fn1.prototype.isPrototypeOf(object), 'test6-prototype');
-  console.assert(typeof object.sayHi === 'function', 'test6-function');
-};
-const test7 = (message: string) => {
-  console.log(message);
-  const fn1 = function (this: any, p1: any, p2: any) {
-    this.p1 = p1;
-    this.p2 = p2;
-  };
-  fn1.prototype.sayHi = function () {};
-  const object1 = new (fn1 as any)('x', 'y');
-  const fn2 = fn1.myBind(object1, 1, 2);
-  const object2 = fn2();
-  console.assert(object2 === undefined, 'object为空');
-  console.assert(object1.p1 === 1);
-  console.assert(object1.p2 === 2);
-};
-
-test5('fn.bind 传入p1,p2，通过new执行返回函数后实例上有p1和p2');
-test6('new 的时候绑定p1,p2, 并且fn1有prototype.sayHi');
-test7('不用new,但是用类似的对象');
 export {};
