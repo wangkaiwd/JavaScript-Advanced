d11c1358d2c9662b71dbf451565ad1389821f943
wangkaiwd <1162486176@qq.com>
2019-09-21 19:26:55 +0800

    feat: first version code and test case

--- principle/promise/src/index.ts
+++ principle/promise/src/index.ts
@@ -1,6 +1,26 @@
+type ResolveFn = (value?: unknown) => void
+type RejectFn = (value?: unknown) => void
 class MyPromise {
-  constructor (execute: (resolve: (value?: unknown) => void, reject: (value?: unknown) => void) => void) {
+  success: ResolveFn | undefined = undefined;
+  fail: RejectFn | undefined = undefined;
+
+  constructor (execute: (resolve: ResolveFn, reject: RejectFn) => void) {
     if (typeof execute !== 'function') {throw new Error('参数只能是函数');}
+    execute(() => {
+      // setTimeout会在then方法执行后再执行
+      setTimeout(() => {
+        this.success && this.success();
+      });
+    }, () => {
+      setTimeout(() => {
+        this.fail && this.fail();
+      });
+    });
+  }
+
+  then (success: ResolveFn, fail?: RejectFn) {
+    this.success = success;
+    this.fail = fail;
   }
 }
 
--- principle/promise/test/index.spec.ts
+++ principle/promise/test/index.spec.ts
@@ -15,10 +15,37 @@ describe('MyPromise', () => {
       new MyPromise();
     });
   });
-  it('new Promise(fn) 会生成一个对象，对象有.then方法', () => {
-
-  });
   it('new Promise(fn) 中传入的fn会立即执行', () => {
-
+    let called = false;
+    const promise = new MyPromise((resolve, reject) => {
+      called = true;
+    });
+    assert.isTrue(called);
   });
+  it('new Promise(fn) 中的fn执行的时候接受resolve和reject俩个函数', () => {
+    const promise = new MyPromise((resolve, reject) => {
+      assert.isFunction(resolve);
+      assert.isFunction(reject);
+    });
+  });
+  it('new Promise(fn) 会生成一个对象，对象有.then方法', () => {
+    const promise = new MyPromise(() => {});
+    assert.isFunction(promise.then);
+  });
+  it('promise.then(success) 中的 success 会在resolve被调用的时候执行', (done) => {
+    let called = false;
+    const promise = new MyPromise((resolve, reject) => {
+      resolve();
+      setTimeout(() => {
+        assert.isTrue(called);
+        done();
+      });
+    });
+    promise.then(() => {
+      called = true;
+    });
+  });
+  // it('', function () {
+  //
+  // });
 });
